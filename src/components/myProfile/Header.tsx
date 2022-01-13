57fe56825756cc673c39a08399ee57f7a2e0e36f
HyunHo Lee <dlgusgh2001@naver.com>
2022-01-13 20:57:12 +0900

    ✨로그아웃 모달 생성

--- src/components/myProfile/Header.tsx
+++ src/components/myProfile/Header.tsx
@@ -6,10 +6,18 @@ import { MyProfileInfo } from "./Info";
 
 export const MyProfileHeader = () => {
   const [myProfileModal, setMyProfileModal] = useState(false);
+  const [logoutModal, setLogoutModal] = useState(false);
   const openMyProfileModal = () => {
     setMyProfileModal(true);
   };
   const closeMyProfileModal = () => {
+    setMyProfileModal(false);
+  };
+  const openLogoutModal = () => {
+    setLogoutModal(true);
+  };
+  const closeLogoutModal = () => {
+    setLogoutModal(false);
     setMyProfileModal(false);
   };
   return (
@@ -26,14 +34,34 @@ export const MyProfileHeader = () => {
         {myProfileModal && (
           <MyProfileModal>
             <li>
-              <button>설정 및 개인정보</button>{" "}
+              <button>설정 및 개인정보</button>
             </li>
             <li>
-              <button>로그아웃</button>
+              <button onClick={openLogoutModal}>로그아웃</button>
             </li>
           </MyProfileModal>
         )}
+        {logoutModal && (
+          <LogOutModal>
+            <LogOutModalContainer>
+              <LogoutText>로그아웃하시겠어요?</LogoutText>
+              <LogOutBtnContainer>
+                <div onClick={closeLogoutModal}>
+                  <LogoutCancel>취소</LogoutCancel>
+                </div>
+                <div>
+                  <Logout>로그아웃</Logout>
+                </div>
+              </LogOutBtnContainer>
+            </LogOutModalContainer>
+          </LogOutModal>
+        )}
       </Nav>
+      <MyProfileInfo />
+      <MyProfileInfo />
+      <MyProfileInfo />
+      <MyProfileInfo />
+      <MyProfileInfo />
       <MyProfileInfo />
     </section>
   );
@@ -80,4 +108,51 @@ const ModalBackground = styled.div`
   height: 90vh;
   background-color: #777;
   opacity: 0.3;
+`;
+const LogOutModal = styled.div`
+  position: absolute;
+  top: 50vh;
+  left: 0;
+`;
+const LogOutModalContainer = styled.div`
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+const LogoutText = styled.p`
+  text-align: center;
+  width: 200px;
+  height: 40px;
+  background-color: #fff;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+  font-size: 12px;
+  padding: 15px 0;
+`;
+const LogOutBtnContainer = styled.div`
+  border-top: 0.5px solid #dbdbdb;
+  width: 200px;
+  display: flex;
+`;
+const LogoutCancel = styled.p`
+  text-align: center;
+  padding-top: 13px;
+  width: 100px;
+  height: 40px;
+  font-size: 10px;
+  border-bottom-left-radius: 10px;
+  border-right: 0.5px solid #dbdbdb;
+  background-color: #fff;
+`;
+
+const Logout = styled.p`
+  text-align: center;
+  padding-top: 13px;
+  width: 100px;
+  height: 40px;
+  font-size: 10px;
+  border-bottom-right-radius: 10px;
+  background-color: #fff;
 `;
