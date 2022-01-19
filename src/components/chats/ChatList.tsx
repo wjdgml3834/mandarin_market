5a1218d19985586716635ac3a7fd724e6b131567
HyunHo Lee <dlgusgh2001@naver.com>
2022-01-19 23:44:37 +0900

    ✨동적 라우팅 고려

--- src/components/chats/ChatList.tsx
+++ src/components/chats/ChatList.tsx
@@ -22,163 +22,19 @@ const chatData = [
     read: false,
   },
   {
-    id: 0,
+    id: 2,
     src: "",
-    name: "김정희",
-    content: "리엑트 너무 쉽다~~",
-    createdAt: "2022.01.19",
+    name: "친구1",
+    content: "ㅇㅋㄷㅋ",
+    createdAt: "2022.01.20",
     read: true,
   },
   {
-    id: 1,
-    src: "https://cdn.pixabay.com/photo/2016/04/07/10/57/skull-1313600_960_720.png",
-    name: "최성이",
-    content: "코딩이 제일 쉬웠어요. 현호님도 저처럼 코딩이 쉬우신가요??",
-    createdAt: "2022.01.19",
-    read: false,
-  },
-  {
-    id: 0,
+    id: 3,
     src: "",
-    name: "김정희",
-    content: "리엑트 너무 쉽다~~",
-    createdAt: "2022.01.19",
-    read: true,
-  },
-  {
-    id: 1,
-    src: "https://cdn.pixabay.com/photo/2016/04/07/10/57/skull-1313600_960_720.png",
-    name: "최성이",
-    content: "코딩이 제일 쉬웠어요. 현호님도 저처럼 코딩이 쉬우신가요??",
-    createdAt: "2022.01.19",
-    read: false,
-  },
-  {
-    id: 0,
-    src: "",
-    name: "김정희",
-    content: "리엑트 너무 쉽다~~",
-    createdAt: "2022.01.19",
-    read: true,
-  },
-  {
-    id: 1,
-    src: "https://cdn.pixabay.com/photo/2016/04/07/10/57/skull-1313600_960_720.png",
-    name: "최성이",
-    content: "코딩이 제일 쉬웠어요. 현호님도 저처럼 코딩이 쉬우신가요??",
-    createdAt: "2022.01.19",
-    read: false,
-  },
-  {
-    id: 0,
-    src: "",
-    name: "김정희",
-    content: "리엑트 너무 쉽다~~",
-    createdAt: "2022.01.19",
-    read: true,
-  },
-  {
-    id: 1,
-    src: "https://cdn.pixabay.com/photo/2016/04/07/10/57/skull-1313600_960_720.png",
-    name: "최성이",
-    content: "코딩이 제일 쉬웠어요. 현호님도 저처럼 코딩이 쉬우신가요??",
-    createdAt: "2022.01.19",
-    read: false,
-  },
-  {
-    id: 0,
-    src: "",
-    name: "김정희",
-    content: "리엑트 너무 쉽다~~",
-    createdAt: "2022.01.19",
-    read: true,
-  },
-  {
-    id: 1,
-    src: "https://cdn.pixabay.com/photo/2016/04/07/10/57/skull-1313600_960_720.png",
-    name: "최성이",
-    content: "코딩이 제일 쉬웠어요. 현호님도 저처럼 코딩이 쉬우신가요??",
-    createdAt: "2022.01.19",
-    read: false,
-  },
-  {
-    id: 0,
-    src: "",
-    name: "김정희",
-    content: "리엑트 너무 쉽다~~",
-    createdAt: "2022.01.19",
-    read: true,
-  },
-  {
-    id: 1,
-    src: "https://cdn.pixabay.com/photo/2016/04/07/10/57/skull-1313600_960_720.png",
-    name: "최성이",
-    content: "코딩이 제일 쉬웠어요. 현호님도 저처럼 코딩이 쉬우신가요??",
-    createdAt: "2022.01.19",
-    read: false,
-  },
-  {
-    id: 0,
-    src: "",
-    name: "김정희",
-    content: "리엑트 너무 쉽다~~",
-    createdAt: "2022.01.19",
-    read: true,
-  },
-  {
-    id: 1,
-    src: "https://cdn.pixabay.com/photo/2016/04/07/10/57/skull-1313600_960_720.png",
-    name: "최성이",
-    content: "코딩이 제일 쉬웠어요. 현호님도 저처럼 코딩이 쉬우신가요??",
-    createdAt: "2022.01.19",
-    read: false,
-  },
-  {
-    id: 0,
-    src: "",
-    name: "김정희",
-    content: "리엑트 너무 쉽다~~",
-    createdAt: "2022.01.19",
-    read: true,
-  },
-  {
-    id: 1,
-    src: "https://cdn.pixabay.com/photo/2016/04/07/10/57/skull-1313600_960_720.png",
-    name: "최성이",
-    content: "코딩이 제일 쉬웠어요. 현호님도 저처럼 코딩이 쉬우신가요??",
-    createdAt: "2022.01.19",
-    read: false,
-  },
-  {
-    id: 0,
-    src: "",
-    name: "김정희",
-    content: "리엑트 너무 쉽다~~",
-    createdAt: "2022.01.19",
-    read: true,
-  },
-  {
-    id: 1,
-    src: "https://cdn.pixabay.com/photo/2016/04/07/10/57/skull-1313600_960_720.png",
-    name: "최성이",
-    content: "코딩이 제일 쉬웠어요. 현호님도 저처럼 코딩이 쉬우신가요??",
-    createdAt: "2022.01.19",
-    read: false,
-  },
-  {
-    id: 0,
-    src: "",
-    name: "김정희",
-    content: "리엑트 너무 쉽다~~",
-    createdAt: "2022.01.19",
-    read: true,
-  },
-  {
-    id: 1,
-    src: "https://cdn.pixabay.com/photo/2016/04/07/10/57/skull-1313600_960_720.png",
-    name: "최성이",
-    content: "코딩이 제일 쉬웠어요. 현호님도 저처럼 코딩이 쉬우신가요??",
-    createdAt: "2022.01.19",
+    name: "친구2",
+    content: "잘먹었다~ 담에 또 보자.",
+    createdAt: "2022.01.21",
     read: false,
   },
 ];
@@ -201,27 +57,28 @@ export const ChatList = () => {
           {chatData.map((chat) => {
             const { id, src, name, content, createdAt, read } = chat;
             return (
-              <ChatItem key={`chatItem-${id}`}>
-                <Link href={`/chats/${id}`}>
-                  <a></a>
-                </Link>
-                <ImgContainer>
-                  {src === "" ? (
-                    <Img
-                      src="./images/ellipse-profile.svg"
-                      alt="프로필 이미지"
-                    />
-                  ) : (
-                    <Img src={src} alt="빈 프로필 이미지" />
-                  )}
-                  {read && <Read />}
-                </ImgContainer>
-                <ChatTextContainer>
-                  <Name>{name}</Name>
-                  <Content>{content}</Content>
-                </ChatTextContainer>
-                <CreatedAt>{createdAt}</CreatedAt>
-              </ChatItem>
+              <Link href={`/chats/${id}`} key={`chatItem-${id}`}>
+                <a>
+                  <ChatItem>
+                    <ImgContainer>
+                      {src === "" ? (
+                        <Img
+                          src="./images/ellipse-profile.svg"
+                          alt="프로필 이미지"
+                        />
+                      ) : (
+                        <Img src={src} alt="빈 프로필 이미지" />
+                      )}
+                      {!read && <Read />}
+                    </ImgContainer>
+                    <ChatTextContainer>
+                      <Name>{name}</Name>
+                      <Content>{content}</Content>
+                    </ChatTextContainer>
+                    <CreatedAt>{createdAt}</CreatedAt>
+                  </ChatItem>
+                </a>
+              </Link>
             );
           })}
         </ChatLists>
