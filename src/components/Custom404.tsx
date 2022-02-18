2dee3cec35b8c096f82a3b4eaf09f0338fe8873c
junghee.kim <wjdgml3834@naver.com>
2022-02-18 14:10:45 +0900

    ✨  404 에러 페이지 추가

---
+++ src/components/Custom404.tsx
@@ -0,0 +1,6 @@
+import styled from "@emotion/styled";
+import React from "react";
+
+export const Custom404Cont = () => {
+  return null;
+};
---
+++ src/pages/404.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { NextPage } from "next";
+import { Custom404Cont } from "../components/Custom404";
+
+const Custom404Page: NextPage = () => {
+  return <Custom404Cont />;
+};
+
+export default Custom404Page;
