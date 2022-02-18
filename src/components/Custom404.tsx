dbbd75e549c401843d3ca286e18ae577f9859ecb
junghee.kim <wjdgml3834@naver.com>
2022-02-18 14:25:39 +0900

    ✨  404 페이지 마크업

--- src/components/Custom404.tsx
+++ src/components/Custom404.tsx
@@ -1,6 +1,15 @@
 import styled from "@emotion/styled";
 import React from "react";
+import Router from "next/router";
+import Image from "next/image";
+import Icon404 from "../img/icon-404.png";
 
 export const Custom404Cont = () => {
-  return null;
+  return (
+    <main>
+      <Image src={Icon404} alt="404 Error Icon" width="150px" height="150px" />
+      <p>페이지를 찾을 수 없습니다 :(</p>
+      <button onClick={() => Router.back()}>이전 페이지</button>
+    </main>
+  );
 };
