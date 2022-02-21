725d277511ab80b6d151b82513e93fd2fccf590c
junghee.kim <wjdgml3834@naver.com>
2022-02-21 20:31:56 +0900

    ✨  메인 홈 화면 api 연동

--- src/components/home/Main.tsx
+++ src/components/home/Main.tsx
@@ -4,10 +4,12 @@ import { CardContainer } from "./CardContainer";
 import { NonFeed } from "../home/Nonfeed";
 import { getSession } from "next-auth/react";
 import { Session } from "next-auth";
+import axios from "axios";
+import { API_ENDPOINT } from "../../constants";
+import { useData } from "../../hooks/useMandarinData";
 
 export const Main = () => {
   const [showing, setShowing] = useState(false);
-
   const [user, setUser] = useState<Session | null>();
   const session = async () => {
     const data = await getSession();
@@ -17,6 +19,32 @@ export const Main = () => {
   useEffect(() => {
     session().then((data: Session | null) => setUser(data));
   }, []);
+
+  // const [user, setUser] = useState<Session | null>();
+  // const session = async () => {
+  //   const data = await getSession();
+  //   console.log(data);
+  //   return data;
+  // };
+
+  // useEffect(() => {
+  //   session().then((data: Session | null) => setUser(data));
+  // }, []);
+
+  const userData = async () => {
+    try {
+      const res: any = await axios.get(API_ENDPOINT + "post/feed/", {
+        headers: {
+          Authorization: `Bearer ${user?.user?.name}`,
+          "Content-type": "application/json",
+        },
+      });
+      console.log(res);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  userData();
 
   return (
     <MainContainer>
@@ -34,3 +62,6 @@ const MainContainer = styled.main`
   overflow-y: scroll;
   padding: 20px 16px 0;
 `;
+function res(res: any) {
+  throw new Error("Function not implemented.");
+}
