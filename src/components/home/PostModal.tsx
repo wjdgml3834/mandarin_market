c4ad4a636ce7c606b47a2c0352488acb947db29b
choisung2 <c0817d@gmail.com>
2022-02-28 17:49:17 +0900

    ♻️ 모달 리팩토링 및 config downlevelIteration 허용

--- src/components/home/PostModal.tsx
+++ src/components/home/PostModal.tsx
@@ -1,11 +1,14 @@
 import styled from "@emotion/styled";
+import Link from "next/link";
+import { COLOR } from "../../constants";
 
 interface ModalData {
+  id: string,
   postModal: boolean;
   openDeleteModal: () => void;
 }
 
-export const PostModal = ({ postModal, openDeleteModal }: ModalData) => {
+export const PostModal = ({ postModal, openDeleteModal, id }: ModalData) => {
   return (
     <div>
       {postModal && (
@@ -14,7 +17,10 @@ export const PostModal = ({ postModal, openDeleteModal }: ModalData) => {
             <button onClick={openDeleteModal}>삭제</button>
           </li>
           <li>
-            <button>수정</button>
+            <Link href={`/post/${id}/edit`}>
+              <EditBtn>수정</EditBtn>
+            </Link>
+            {/* <button>수정</button> */}
           </li>
         </Container>
       )}
@@ -26,24 +32,50 @@ const Container = styled.ul`
   position: fixed;
   bottom: 0;
   left: 0;
+  padding: 36px 0 14px;
   background-color: #fff;
   border-top-right-radius: 10px;
   border-top-left-radius: 10px;
-  z-index: 10;
+  z-index: 100;
   &::before {
-    display: block;
+    position: absolute;
     content: "";
+    top: 16px;
+    left: 50%;
     width: 50px;
     height: 4px;
     border-radius: 5px;
     background-color: #dbdbdb;
-    margin: 16px auto;
+    margin-top: 3px;
+    transform: translateX(-50%);
   }
   button {
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
     width: 100vw;
-    height: 46px;
+    height: 42px;
     font-size: 14px;
-    padding: 20px;
-    text-align: start;
+    padding-left: 26px;
+    line-height: 18px;
+    cursor: pointer;
+    &:hover {
+      color: ${COLOR.orange}
+    }
   }
 `;
+
+const EditBtn = styled.a`
+  padding-left: 26px; 
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  width: 100vw;
+  height: 46px;
+  font-size: 14px;
+  line-height: 18px;
+  cursor: pointer;
+  &:hover {
+    color: ${COLOR.orange}
+  }
+`
